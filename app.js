aebf2710ef22826c4c23d49822fb8102666f9bbd
YD1234564789 <a8111202003@gmail.com>
2023-09-25 17:19:26 +0800

    feat: initialize api server

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
 const SESSION_SECRET = 'secret'
-const routes = require('./routes')
+const { pages } = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(routes)
+app.use(pages)
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
